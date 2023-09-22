008ba03b9b12d0d898a6321b71f48e0720c18f8e
nx2141 <super_red_block@yahoo.co.jp>
2023-09-22 17:49:26 +0900

    post投稿まで追加

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Head from 'next/head'
+import { useRouter } from "next/router";
+import apiClient from "@/lib/apiClient";
+import { useAuth } from "@/context/auth";
 
-const login = () => {
+const Login = () => {
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+  
+  
+    const router = useRouter();
+
+    const {login} = useAuth();
+  
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      //ログインを行うためAPIを叩く
+      try{
+          const response = await apiClient.post("/auth/login",{
+              email,
+              password,
+          });
+          const token = response.data.token;
+          login(token);
+          router.push("/");
+      }catch(err){
+          alert("入力内容が正しくありません。");
+      }
+    };
+  
+
+
+
   return (
     <div
       style={{ height: "88vh" }}
@@ -17,7 +47,7 @@ const login = () => {
       </div>
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div>
               <label
                 htmlFor="email"
@@ -32,6 +62,9 @@ const login = () => {
                 autoComplete="email"
                 required
                 className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 text-base focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setEmail(e.target.value)
+                  }
               />
             </div>
             <div className="mt-6">
@@ -48,6 +81,9 @@ const login = () => {
                 autoComplete="current-password"
                 required
                 className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 text-base focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setPassword(e.target.value)
+                  }
               />
             </div>
             <div className="mt-6">
@@ -65,4 +101,4 @@ const login = () => {
   );
 };
 
-export default login;
+export default Login;
