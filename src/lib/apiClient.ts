4144f35378bb6c595961b663d3db0f594fa38906
nx2141 <super_red_block@yahoo.co.jp>
2023-10-12 02:08:25 +0900

    dev

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -3,7 +3,8 @@ import axios from "axios";
 const apiClient = axios.create({
     baseURL:"http://localhost:5000/api",
     headers:{
-        "Content-type":"application/json"
+        "Content-type":"application/json",
+        // "Authorization":`Beare{token}`
     },
 });
 
